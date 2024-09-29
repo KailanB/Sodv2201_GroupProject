f601337e3e9ba151a86fd90cb0aa56b0b7120ccc
KailanB <kailanbates@gmail.com>
2024-09-29 14:53:44 -0600

    Made Search div component for programs page

---
+++ src/components/ProgramsPage/ProgramSearch.component.jsx
@@ -0,0 +1,18 @@
+import './ProgramSearch.style.css';
+
+const ProgramSearch = () => {
+
+
+
+    return (
+
+        <div className='standardInput'>
+            <input></input>
+            <button className='standardButton'>Search</button>
+        </div>
+
+    );
+};
+
+
+export default ProgramSearch;
