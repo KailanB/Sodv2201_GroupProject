267661602458b5cad125195fde4a77f63a2fdc59
KailanB <kailanbates@gmail.com>
2024-09-29 14:53:11 -0600

    Using App.js to test each component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import Header from './components/Common/Header.component.jsx';
 import Footer from './components/Common/Footer.component.jsx';
 
+import ProgramDiv from './components/ProgramsPage/ProgramDiv.component.jsx';
+import ProgramSearch from './components/ProgramsPage/ProgramSearch.component.jsx';
+
+import SignUpPage from './components/SignUpPage/SignUpPage.component.jsx';
+
 
 function App() {
 
@@ -11,10 +16,20 @@ function App() {
     <div className="mainContent">
 
       <Header title="Home"></Header>
+      <SignUpPage />
+      
       <Footer></Footer>
 
     </div>
   );
 }
+
+/*
+
+<ProgramSearch />
+<ProgramDiv programTitle="Certificate" term="Fall" startDate="2024-09-03" endDate="2024-12-20" length="6 months" description="this certificate programs offers a fast track into the world of software development." fee="80,000" code="445"/>
+
+
+*/
 
 export default App;
