5de4a4eed98689048a49e3cb89a0df7a410e798e
KailanB <kailanbates@gmail.com>
2024-10-06 18:32:17 -0600

    User list is only for output testing to check for accurate collection of data

---
+++ src/components/SignUpPage/UserList.component.jsx
@@ -0,0 +1,35 @@
+// this component is used for testing purposes ONLY
+// we can borrow the style to output course and program information
+
+
+import React from 'react';
+
+function UserList({users}) {
+
+    return (
+
+        <ul>
+            {users.map((user, index) => (
+                <li key={index}>
+                    Name: {user.firstName} {user.lastName} <br />
+                    Email: {user.email} <br />
+                    Phone: {user.phone} <br />
+                    Birthday: {user.birthday} <br />
+                    Department: {user.department} <br />
+                    Program: {user.program} <br />
+                    Username: {user.userName} <br />
+                    Password: {user.password} <br />
+
+                </li>
+            ))}
+
+        </ul>
+
+
+    );
+
+};
+
+
+
+export default UserList;
