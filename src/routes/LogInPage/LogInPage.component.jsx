6e1431de47b88bfc1e07901026fc191716081cf8
KailanB <kailanbates@gmail.com>
2024-10-09 19:19:06 -0600

    Added log in functionality with cookies
    used the same form data handling as we learned in class.
    
    then we pull local storage data and check that the email exists in the local data, if the email exists we check for matching passwords. If everything checks out we set user cookie data so that profile information etc. can be displayed
    
    right now I did a lot of the cookie stuff manually without functions just to get some things working.
    
    The page currently does not change locations after log in so it's likely working just not relocating yet. This can be added.
    
    If email is not found in local data OR if passwords do not match there is an alert.

---
+++ src/routes/LogInPage/LogInPage.component.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import './LogInPage.style.css';
+
+const LogInPage = () => {
+
+    const[formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+
+        const {name, value} = e.target;
+        setFormData({
+            ...formData, 
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAddUser(formData);
+        setFormData({
+            email: '',
+            password: ''
+        });
+    }
+
+    const onAddUser = (user) => {
+
+        // searches the local storage for the email that is attempting to be registered
+        const users = JSON.parse(localStorage.getItem('users'));
+        let userExists = users.find(savedUser => savedUser.email.toLowerCase() === user.email.toLowerCase());
+        // if user exists check that the correct password was used
+        if(userExists)
+        {
+            // if passwords match
+            if(userExists.password == user.password)
+            {
+                let days = 1; // update these variables to set cookie expiration
+                // let hours = 5;
+                const expireDate = new Date(); 
+                // create user cookie with users email
+                expireDate.setTime(expireDate.getTime() + (days * 24 * 60 * 60 * 1000));
+                document.cookie = "userEmail=" + userExists.email.toLowerCase() + ";" + expireDate + ";path=/";
+            }
+            else
+            {
+                alert("That password is incorrect sorry!");
+            }
+        }
+        else
+        {
+            alert("Sorry. Email does not exist!");
+        }
+    };
+
+
+    return (
+        <div>
+            <div className="logInPageMain">
+                <div className="logInPageHalf">
+                    <div className="logInDiv">
+                        
+                        <form onSubmit={handleSubmit}>
+                            <div className='labelInputDiv'>
+                                <label>Email: </label>
+                                <input 
+                                type="email" 
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="standardInput" 
+                                required></input>
+                            </div>
+                            <div className='labelInputDiv'>
+                                <label>Password: </label>
+                                <input 
+                                type="text" 
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="standardInput" 
+                                required></input>
+                            </div>
+                            <button className="standardButton">Reset</button>
+                            <button className="standardButton">Cancel</button>
+                            <input type='submit' value="Log In" className="standardButton"/> 
+                        </form>
+                    </div>
+                </div>
+                
+                <div className="logInPageHalf">
+                    <p>Don't have an account? Make one here!</p>
+                </div>
+            </div>
+        </div>
+
+        
+        
+
+
+    );
+};
+
+
+export default LogInPage;
