84dd4227772ab664972c6b71c8b9ced13c12331c
KailanB <kailanbates@gmail.com>
2024-10-09 19:25:44 -0600

    Updated signup page to load local storage user data
    create users with useState
    
    useEffect #1, on load we collect local storage userdata and and then save it to our users state.
    
    useEffect #2 fires when users state is updated. It checks if user data already exists, if not this does nothing extra.
    If users data exists (meaning nothing went wrong with data collection or addition) then it sets the local storage with the new data including the new user
    
    addUser function now checks the localstorage for already existing email, if not it adds the user to the local storage.
    
    No page change on sign up right now.

---
+++ src/routes/SignUpPage/SignUpPage.component.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+
+import './SignUpPage.style.css';
+import SignUpForm from './SignUpForm.component.jsx';
+import UserList from './UserList.component.jsx';
+
+const SignUpPage = () => {
+
+
+
+    const[users, setUsers] = useState([]);
+
+    // for using local storage https://www.freecodecamp.org/news/how-to-use-localstorage-with-react-hooks-to-set-and-get-items/
+    // needed much more info on this as nothing was working
+    // https://stackoverflow.com/questions/75855282/why-does-the-local-storage-get-deleted-when-i-refresh
+    useEffect(() => {
+        // this effect runs on first load of the page
+        // pulls data from local storage
+        const savedUsers = JSON.parse(localStorage.getItem('users'));
+        // checks if data exists. If so, then set our users with local storage data
+        if(savedUsers)
+        {
+            setUsers(savedUsers);
+        }
+    }, []);
+
+    // this effect runs every time users state is updated aka this runs when we add a new user through addUser function
+    useEffect(() => {
+
+        // first check that the users array actually contains data. If it does NOT we do not want to save nothing to local storage
+        // this is a fail safe in case anything loads wonky, and the first effect runs after this one.
+        if(users.length > 0)
+        {
+            localStorage.setItem('users', JSON.stringify(users));
+        }   
+    }, [users]);
+
+    const addUser = (user) => {
+
+        // searches the local storage for the email that is attempting to be registered
+        let userExists = users.find(savedUser => savedUser.email.toLowerCase() === user.email.toLowerCase())
+        // if no such user exists we can go ahead and add the user to our storage
+        if(!userExists)
+        {
+            user.email.toLowerCase();
+            setUsers((currentUserSet => [...currentUserSet, user]));
+        }
+        // else notify the user and DO not add a duplicate email.
+        else
+        {
+            alert("Sorry. Email already in use!")
+        }
+      
+    };
+
+    return (
+       
+        <div>
+            <div className='signUpDiv'>
+                <h3>Sign up here!</h3>
+                <SignUpForm onAddUser={addUser}/>
+                <div>
+                    {/* <UserList users={users}/> */}
+                    {/* {JSON.parse(localStorage.getItem('users')).map((user, index) => (
+                            <li key={index}>
+                                Name: {user.firstName} {user.lastName} <br />
+                                Email: {user.email} <br />
+                                Password: {user.password} <br />
+                            </li>
+        
+                        ))} */}
+                </div>
+            </div>
+        </div>
+        
+
+
+    );
+
+
+}
+
+
+
+
+
+
+
+
+export default SignUpPage;
