b9c1b49adeb2a9bc41fc88373990bbcd9827f83d
KailanB <kailanbates@gmail.com>
2024-10-13 14:42:55 -0600

    added new navbar component for student users
    This is current working when logged in as student. I think we will have to create some dummy data for pre-defined admins since there is no way to create an admin account.
    
    Also I think I will change the way this works and put all the checks within the nav bar component then return the correct links for the type of user.
    
    Currently the header component checks type of user then returns the correct burger menu with navbar component

--- src/components/Common/Header.component.jsx
+++ src/components/Common/Header.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Header.style.css';
 import NavBarBasic from './NavBarBasic.component';
+import NavBarStudent from './NavBarStudent.component';
 
 const Header = (props) => {
 
@@ -11,22 +12,74 @@ const Header = (props) => {
     }
 
     const {title} = props;
-    // adding hamburger menu https://www.w3schools.com/howto/howto_css_menu_icon.asp
-    return (
-        <div className="header">
-            <p>WSA</p>
-            <h1>{title}</h1>
-            <div>
-                <div className="burgerMenu" onClick={toggleNavBar}> 
-                    <div className="burgerMenuBar"></div>
-                    <div className="burgerMenuBar"></div>
-                    <div className="burgerMenuBar"></div>
-                </div>
-                {isVisible && <NavBarBasic home="home" />}
-            </div>
+    
+    // localStorage.clear();
 
-        </div>
-    );
+    // const users = JSON.parse(localStorage.getItem('users'));
+    // alert(document.cookie);
+    let cookieName = "userEmail=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let myEmail = '';
+    // get email from cookie
+    myEmail = decodedCookie.substring(cookieName.length);
+    // get user data from storage
+    const users = JSON.parse(localStorage.getItem('users'));
+    let currentUser;
+    // if data exists
+    if(users)
+    {
+        // find user with matching email
+        currentUser = users.find(savedUser => savedUser.email.toLowerCase() === myEmail.toLowerCase());
+    }
+    // if user is logged in and cookie email exists
+    if(myEmail)
+    {
+        
+        // alert(currentUser.status);
+        // check if current user has status addmin or student
+        if(currentUser.status == "Admin")
+        {
+
+        }
+        else if (currentUser.status == "Student")
+        {
+            // adding hamburger menu https://www.w3schools.com/howto/howto_css_menu_icon.asp
+            return (
+                <div className="header">
+                    <p>WSA</p>
+                    <h1>{title}</h1>
+                    <div>
+                        <div className="burgerMenu" onClick={toggleNavBar}> 
+                            <div className="burgerMenuBar"></div>
+                            <div className="burgerMenuBar"></div>
+                            <div className="burgerMenuBar"></div>
+                        </div>
+                        {isVisible && <NavBarStudent home="home" />}
+                    </div>
+        
+                </div>
+            );
+        }
+    }
+    else
+    {
+        return (
+            <div className="header">
+                <p>WSA</p>
+                <h1>{title}</h1>
+                <div>
+                    <div className="burgerMenu" onClick={toggleNavBar}> 
+                        <div className="burgerMenuBar"></div>
+                        <div className="burgerMenuBar"></div>
+                        <div className="burgerMenuBar"></div>
+                    </div>
+                    {isVisible && <NavBarBasic home="home" />}
+                </div>
+    
+            </div>
+        );
+    }
+    
 
 };
 
