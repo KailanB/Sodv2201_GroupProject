0289ee54ecfe0d1aca4355244995cd5c4768a788
KailanB <kailanbates@gmail.com>
2024-10-14 18:25:59 -0600

    added contact page form submit functions

--- src/routes/ContactPage/ContactPage.component.jsx
+++ src/routes/ContactPage/ContactPage.component.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './ContactPage.style.css';
 
 const ContactPage = () => {
+
+
+    const[formData, setFormData] = useState({
+        fullName: '', 
+        email: '',
+        message: '',
+
+    });
+
+    const handleChange = (e) => {
+
+        const {name, value} = e.target;
+        setFormData({
+            ...formData, 
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAddUser(formData);
+        setFormData({
+            fullName: '', 
+            email: '',
+            message: '',
+
+        });
+    }
+
+    const onAddUser = (message) => {
+
+       
+        // I was having an unknown issue earlier we may have to use this version
+        // const savedMessages = JSON.parse(localStorage.getItem('messages'));
+        // if(!savedMessages)
+        // {
+        //     savedMessages = [];
+        // }
+
+
+        // pull local storage of messages
+        const savedMessages = JSON.parse(localStorage.getItem('messages')) || [];
+        //if storage exists
+        // add new message
+        savedMessages.push(message);
+        // save updated array
+        localStorage.setItem('messages', JSON.stringify(savedMessages));
+      
+    };
+
+
     return (
         <div className="admin-page">
             <h3>Contact Us</h3>
@@ -28,18 +79,39 @@ const ContactPage = () => {
                 </div>
 
                 {/* Right Box - Contact Form */}
-                <form className="form-group" action="/submit" method="POST">
+                <form className="form-group" action="/submit" method="POST" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="fullName">Full Name:</label>
-                        <input type="text" id="fullName" className="standardInput" required />
+                        <input 
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        type="text" 
+                        id="fullName" 
+                        className="standardInput" 
+                        required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" className="standardInput" required />
+                        <input 
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        type="email" 
+                        id="email" 
+                        className="standardInput" 
+                        required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="message">Message:</label>
-                        <textarea id="message" className="standardInput" rows="4" required />
+                        <textarea 
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        id="message" 
+                        className="standardInput" 
+                        rows="4" 
+                        required />
                     </div>
                     <button type="submit" className="submitButton">Send Message</button>
                 </form>
