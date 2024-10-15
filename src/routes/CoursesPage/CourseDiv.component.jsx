060f779aaf7d589e8c47905f0aa052901a920f2e
KailanB <kailanbates@gmail.com>
2024-10-14 19:21:41 -0600

    added courses page and course div

---
+++ src/routes/CoursesPage/CourseDiv.component.jsx
@@ -0,0 +1,27 @@
+import './CoursePage.style.css';
+
+const CourseDiv = (props) => {
+
+
+    
+    // const {fullName, email, message} = props;
+
+    return (
+
+
+        <div>
+            
+        </div>
+
+
+        // <div className="messageDiv" >
+        //     <h4>From {fullName}</h4>
+        //     <p>Email: {email}</p>
+        //     <p>Message: {message}</p>
+        // </div>
+
+    );
+};
+
+
+export default CourseDiv;
---
+++ src/routes/CoursesPage/CoursesPage.component.jsx
@@ -0,0 +1,48 @@
+import {React, useState, useEffect} from 'react';
+import './CoursePage.style.css';
+import CourseDiv from './CourseDiv.component.jsx';
+
+
+
+
+
+const CoursesPage = () => {
+
+
+
+    // const [messages, setMessages]= useState([]);
+
+    // useEffect(() => {
+
+    //     const savedMessages = JSON.parse(localStorage.getItem('messages'));
+
+    //     if(savedMessages)
+    //     {
+    //         setMessages(savedMessages);
+    //     }
+
+    // }, []);
+
+
+
+    return (
+
+        <div>
+
+        </div>
+
+        // <div>
+        //     {messages.map((message, index) => (
+        //         <div key={index}>
+        //             <CourseDiv fullName={message.fullName} email={message.email} message={message.message}/>
+        //             <br />
+        //         </div>
+                
+        //     ))}
+        // </div>
+
+    );
+};
+
+
+export default CoursesPage;
