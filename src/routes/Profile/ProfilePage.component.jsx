a0b47a3369acea9e91c7ddd3ac215048f7594cd4
KailanB <kailanbates@gmail.com>
2024-10-14 18:26:47 -0600

    remove cookie function and pulled function from utilities

--- src/routes/Profile/ProfilePage.component.jsx
+++ src/routes/Profile/ProfilePage.component.jsx
@@ -1,53 +1,39 @@
 import React, { useState, useEffect } from 'react';
 
 import ProfileDiv from './ProfileDiv.component.jsx';
+import { GetCookieByName } from '../../Utilities.js';
 
 function ProfilePage() {
 
 
+    
     const[user, setUser] = useState([]);
+
+    
 
     useEffect(() => {
 
-        let cookieName = "userEmail=";
-    
-        let decodedCookie = decodeURIComponent(document.cookie);
         
-        // let cookieValues = decodedCookie.split(";");
-        let myEmail = '';
-        myEmail = decodedCookie.substring(cookieName.length);
-        // alert(myEmail);
-        // for (let i = 0; i < cookieValues.length ; i++)
-        // {
-            
-        //     let c = cookieValues[i];
-        //     while (c.charAt(0) === ' ')
-        //     {
-        //         c = c.substring(1);
-                
-        //     }
-        //     if (c.indexOf(name) === 0)
-        //     {
-                
-        //         myEmail = c.substring(name.length, c.length);
-        //     }
-            
-
-        // }
+        let userEmail = GetCookieByName("userEmail=")
 
         const savedUsers = JSON.parse(localStorage.getItem('users'));
-        let userExists = savedUsers.find(savedUser => savedUser.email.toLowerCase() === myEmail.toLowerCase());
+
+       
+        let userExists = savedUsers.find(savedUser => savedUser.email.toLowerCase() === userEmail.toLowerCase());
+
+
         if(userExists)
         {
-
-            setUser(userExists);
-            
+            //alert(userExists.studentId);
+            setUser(userExists);  
         }
     }, []);
 
+    
+
     return (
         <div>
-            <ProfileDiv firstName={user.firstName} lastName={user.lastName} id={user.studentID} email={user.email} phone={user.phone} birthday={user.birthday} department={user.department} program={user.program} userName={user.userName}/>
+            <ProfileDiv firstName={user.firstName} lastName={user.lastName} id={user.studentId} email={user.email} phone={user.phone} birthday={user.birthday} department={user.department} program={user.program} userName={user.userName}/>
         </div>
     );
 }
