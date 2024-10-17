71cefab8ff3e2f90061c310a4f91a6d216f04c12
KailanB <kailanbates@gmail.com>
2024-10-16 18:35:55 -0600

    added functionality to only display courses relevant to the student if a student user is logged in

--- src/routes/CoursesPageNew/CoursesPage.component.jsx
+++ src/routes/CoursesPageNew/CoursesPage.component.jsx
@@ -2,6 +2,8 @@ import {React, useState, useEffect} from 'react';
 import './CoursePageNew.style.css';
 import CourseDiv from './CourseDiv.component.jsx';
 
+import { GetCookieByName } from '../../Utilities.js';
+
 
 
 
@@ -9,16 +11,74 @@ import CourseDiv from './CourseDiv.component.jsx';
 const CoursesPage = () => {
 
     const [courses, setCourses] = useState([]);
+
+    const [programs, setPrograms] = useState([]);
+
+
     const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => { 
 
-        const savedCourses = JSON.parse(localStorage.getItem("courses"));
-        if (savedCourses) {
-            setCourses(savedCourses);
+        
+
+    }, []);
+
+    // useEffect(() => { 
+
+    //     const savedCourses = JSON.parse(localStorage.getItem("courses"));
+    //     if (savedCourses) {
+    //         setCourses(savedCourses);
+    //     }
+
+    // }, []);
+
+    useEffect(() => { 
+
+        const savedPrograms = JSON.parse(localStorage.getItem("programs"));
+
+        setPrograms(savedPrograms);
+
+        const savedCourses = [];
+        
+        savedPrograms.forEach(program => 
+            program.courses.forEach(course =>
+                
+                savedCourses.push(course)
+            )
+        )
+
+        setCourses(savedCourses);
+
+
+    }, []);
+
+
+    const[user, setUser] = useState([]);
+
+    useEffect(() => {
+
+        
+        let userEmail = GetCookieByName("userEmail=")
+        const savedUsers = JSON.parse(localStorage.getItem('users'));
+        let userExists = savedUsers.find(savedUser => savedUser.email.toLowerCase() === userEmail.toLowerCase());
+        
+        if(userExists)
+        {
+            //alert(userExists.department + " " + userExists.program)
+            setUser(userExists); 
+
+            // if user logged in is a student filter to only display courses available for the users department and program
+            if(userExists.status === "Student")
+            {
+                const userCourses = courses.filter(course => (
+                course.Department.toLowerCase() === user.department.toLowerCase()) &&
+                (course.Program.toLowerCase() === user.program.toLowerCase())
+                )
+                // set courses to filtered options for displaying
+                setCourses(userCourses);
+            }
+            
         }
-
-
 
 
 
@@ -42,8 +102,8 @@ const CoursesPage = () => {
 
     return (
 
-        <div>
-            <div className="courses-page">
+    <div>
+        <div className="courses-page">
             <h3>Course Registration</h3>
             <input 
                 type="text" 
@@ -54,18 +114,18 @@ const CoursesPage = () => {
             />
         </div>
 
-    <div className="course-list">
-                    {courses.filter(course => 
-                        course.CourseName.toLowerCase().includes(searchTerm.toLowerCase()) || 
-                        course.CourseCode.toLowerCase().includes(searchTerm.toLowerCase())
-                    ).map((course, index) => (
-                            <div key = {index }>
-                            <CourseDiv Course = {course}/>
-                            <br></br>
-                                </div>
+        <div className="course-list">
+            {courses.filter(course => 
+            (course.CourseName.toLowerCase().includes(searchTerm.toLowerCase()) || 
+            course.CourseCode.toLowerCase().includes(searchTerm.toLowerCase()))
+            ).map((course, index) => (
+                <div key = {index }>
+                    <CourseDiv Course = {course}/>
+                    <br />
+                </div>
                     ))}
-                    </div>
-                    </div>
+        </div>
+    </div>
 
 
 
