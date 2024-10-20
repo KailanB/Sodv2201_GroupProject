40ab2ab2f1ef11a7d6b965799c248dea4f75f94d
KailanB <kailanbates@gmail.com>
2024-10-20 15:14:31 -0600

    added better dummy data descriptions

--- src/view/LoadDummyData.js
+++ src/view/LoadDummyData.js
@@ -2,15 +2,15 @@
 
 export const LoadData = () => {
 
-    //  localStorage.clear();
+    // localStorage.clear();
     const allPrograms = JSON.parse(localStorage.getItem('programs')) || [];
     if (allPrograms.length < 1)
     {
         // create an indivudla file for importing data to keep the files clean
         let dummyData = [
-            {department: "Software Development", program: "Diploma", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "This is programming", fee: "50,000", code: "23523", courses: [{CourseName: "Sodv", CourseCode: 2202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the ins and outs of object oriented programming", CourseId: 1}, {CourseName: "Sodv", CourseCode: 2201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the fundamentals of React in javascript.", CourseId: 2}, {CourseName: "Data", CourseCode: 2201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the fundamentals of database administration.", CourseId: 3}]},
-            {department: "Software Development", program: "Certificate", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "This is programming basics", fee: "30,000", code: "23524", courses: [{CourseName: "Sodv", CourseCode: 1101, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the basics of programming using C++", CourseId: 4}, {CourseName: "Tech", CourseCode: 1101, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the fundamentals of web programming.", CourseId: 5}, {CourseName: "Tech", CourseCode: 1102, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the fundamentals of IoT.", CourseId: 6}]},
-            {department: "Software Development", program: "Post-Diploma", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "This is programming advanced", fee: "75,000", code: "23525", courses: [{CourseName: "Sodv", CourseCode: 3202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced object oriented architecture", CourseId: 7}, {CourseName: "Data", CourseCode: 3202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced concepts managing databases.", CourseId: 8}, {CourseName: "Sodv", CourseCode: 3201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced and modern web programming concepts.", CourseId: 9}, {CourseName: "Sodv", CourseCode: 3203, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners the basics of using Unreal Engine.", CourseId: 9}]},
+            {department: "Software Development", program: "Diploma", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "This 2 year diploma course teaches students everything they need to gain a junior developer job. Starting with programming foundation and fundamentals to building full stack web apps, games, and more by completion", fee: "50,000", code: "23523", courses: [{CourseName: "Sodv", CourseCode: 2202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the ins and outs of object oriented programming", CourseId: 1}, {CourseName: "Sodv", CourseCode: 2201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the fundamentals of React in javascript.", CourseId: 2}, {CourseName: "Data", CourseCode: 2201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Diploma", Description: "Teachers learners the fundamentals of database administration.", CourseId: 3}]},
+            {department: "Software Development", program: "Certificate", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "Our certificate program offers students a fast track into the world of programming. Focusing on the basics and fundamentals.", fee: "30,000", code: "23524", courses: [{CourseName: "Sodv", CourseCode: 1101, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the basics of programming using C++", CourseId: 4}, {CourseName: "Tech", CourseCode: 1101, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the fundamentals of web programming.", CourseId: 5}, {CourseName: "Tech", CourseCode: 1102, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Certificate", Description: "Teachers learners the fundamentals of IoT.", CourseId: 6}]},
+            {department: "Software Development", program: "Post-Diploma", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "The Post-Diploma program takes a junior developers skills to the next level. Tackling new concepts and algorithms.", fee: "75,000", code: "23525", courses: [{CourseName: "Sodv", CourseCode: 3202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced object oriented architecture", CourseId: 7}, {CourseName: "Data", CourseCode: 3202, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced concepts managing databases.", CourseId: 8}, {CourseName: "Sodv", CourseCode: 3201, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners advanced and modern web programming concepts.", CourseId: 9}, {CourseName: "Sodv", CourseCode: 3203, Term: "Winter", StartDate: "2025-01-05", EndDate: "2025-04-25", Department:"SoftWare Development", Program: "Post-Diploma", Description: "Teachers learners the basics of using Unreal Engine.", CourseId: 9}]},
             // {department: 'Engineering', program: "Certificate", term: "fall", startDate: "September", endDate: "December", legnth: "6 months", description: "This is Engineering", fee: "20,000", code: "54523", courses: [] }
         ];
         localStorage.setItem('programs', JSON.stringify(dummyData));
@@ -18,106 +18,79 @@ export const LoadData = () => {
 
     const allUsers = JSON.parse(localStorage.getItem('users')) || [];
     // if no user data exists create admin dummy data
-    if(allUsers.length <= 0)
+    if(allUsers.length < 1)
     {
         
-        let user = {
-            firstName: 'Teeny',
-            lastName: 'Tine',
-            email: 'admin@gmail.com',
-            phone: '123-456-1234',
-            birthday: '1900-10-10',
-            department: 'IT',
-            program: '',
-            term: '',
-            userName: 'TT',
-            password: '123',
-            status: 'Admin',
-            studentId: '',
-            courses: []
-        }
-        allUsers.push(user);
-        localStorage.setItem('users', JSON.stringify(allUsers));
-    }
-    else 
-    {
-        
-        // if no such user exists we can go ahead and add the user to our storage
-        let userExists = allUsers.find(savedUser => savedUser.email.toLowerCase() === "ttadmin@gmail.com");
-        if(!userExists)
+            // admin account
+            const users = [{
+                firstName: 'Teeny',
+                lastName: 'Tine',
+                email: 'admin@gmail.com',
+                phone: '123-456-1234',
+                birthday: '1900-10-10',
+                department: 'IT',
+                program: '',
+                term: '',
+                userName: 'TT',
+                password: '123',
+                status: 'Admin',
+                studentId: '',
+                courses: []
+            },
+            // add extra dummy data - students
             {
-                // admin account
-                let users = [{
-                    firstName: 'Teeny',
-                    lastName: 'Tine',
-                    email: 'ttadmin@gmail.com',
-                    phone: '123-456-1234',
-                    birthday: '1900-10-10',
-                    department: 'IT',
-                    program: '',
-                    term: '',
-                    userName: 'TT',
-                    password: '123',
-                    status: 'Admin',
-                    studentId: '',
-                    courses: []
-                },
-                // add extra dummy data - students
-                {
-                    firstName: 'Scram',
-                    lastName: 'Biss',
-                    email: 'scramb@gmail.com',
-                    phone: '133-556-1739',
-                    birthday: '2010-6-10',
-                    department: 'Software Development',
-                    program: 'Diploma',
-                    term: 'Fall',
-                    userName: 'Scramb',
-                    password: '123',
-                    status: 'Student',
-                    studentId: '5',
-                    courses: []
-                },
-                {
-                    firstName: 'Rosa',
-                    lastName: 'Diaz',
-                    email: 'rosadiaz@gmail.com',
-                    phone: '543-555-8643',
-                    birthday: '1990-2-15',
-                    department: 'Software Development',
-                    program: 'Post-Diploma',
-                    term: 'Winter',
-                    userName: 'RD',
-                    password: '123',
-                    status: 'Student',
-                    studentId: '3',
-                    courses: []
-                },
-                {
-                    firstName: 'Michael',
-                    lastName: 'Scott',
-                    email: 'mscott@gmail.com',
-                    phone: '434-325-9809',
-                    birthday: '1970-7-24',
-                    department: 'Software Development',
-                    program: 'Certificate',
-                    term: 'Spring',
-                    userName: 'Mscott',
-                    password: '123',
-                    status: 'Student',
-                    studentId: '8',
-                    courses: []
-                }
-            ]
-
-                // dummy data for admin is good
-        
-                // separate users from admin users 
-                // or we could add privilege access for admins
-                localStorage.setItem('users', JSON.stringify(users));
-    
+                firstName: 'Scram',
+                lastName: 'Biss',
+                email: 'scramb@gmail.com',
+                phone: '133-556-1739',
+                birthday: '2010-6-10',
+                department: 'Software Development',
+                program: 'Diploma',
+                term: 'Fall',
+                userName: 'Scramb',
+                password: '123',
+                status: 'Student',
+                studentId: '5',
+                courses: []
+            },
+            {
+                firstName: 'Rosa',
+                lastName: 'Diaz',
+                email: 'rosadiaz@gmail.com',
+                phone: '543-555-8643',
+                birthday: '1990-2-15',
+                department: 'Software Development',
+                program: 'Post-Diploma',
+                term: 'Winter',
+                userName: 'RD',
+                password: '123',
+                status: 'Student',
+                studentId: '3',
+                courses: []
+            },
+            {
+                firstName: 'Michael',
+                lastName: 'Scott',
+                email: 'mscott@gmail.com',
+                phone: '434-325-9809',
+                birthday: '1970-7-24',
+                department: 'Software Development',
+                program: 'Certificate',
+                term: 'Spring',
+                userName: 'Mscott',
+                password: '123',
+                status: 'Student',
+                studentId: '8',
+                courses: []
             }
+        ]; 
+
+        // dummy data for admin is good
+        // separate users from admin users 
+        // or we could add privilege access for admins
+        localStorage.setItem('users', JSON.stringify(users));
     }
+
 
 
     const allMessages = JSON.parse(localStorage.getItem('messages')) || [];
