15944060b49e9549ad5ebdb08d26750ff69b500b
KailanB <kailanbates@gmail.com>
2024-10-19 20:46:16 -0600

    added dashboard display

--- src/routes/AdminDashboard/AdminDashboard.component.jsx
+++ src/routes/AdminDashboard/AdminDashboard.component.jsx
@@ -1,24 +1,42 @@
 
-import React from 'react';
+import {React, useState, useEffect } from 'react';
 import './AdminDashboard.style.css'; 
+import AdminDashboardDiv from './AdminDashboardDiv.component.jsx';
+import { GetCookieByName } from '../../Utilities.js';
 
-const AdminDashboard = (props) => {
+const AdminDashboard = () => {
 
-  const {name, status } = props;
+  const[users, setUsers] = useState([]);
+
+  const[user, setUser] = useState([]);
+
+  // executed first
+  useEffect(() => {
+    // pull user data from local storage
+    const savedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    setUsers([...savedUsers]);
+
+  }, []);
+
+  // executed after user data is pulled
+  useEffect(() => {
+   
+    // get cookie
+    let userEmail = GetCookieByName("userEmail=");
+    // find mathcing user within saved users and save user data to variable
+    let userExists = users.find(savedUser => savedUser.email.toLowerCase() === userEmail.toLowerCase());
+    if(userExists)
+    {
+      // user user state with collected user info
+      setUser(userExists);
+    }
+  }, [users]);
 
   return (
-    <div className="dashboard-container">
-      <div className="info-box">
-        <h2>Admin Dashboard</h2>
-        <div className="info-item">
-          <strong>Name:</strong> {name}
-        </div>
-        <div className="info-item">
-          <strong>Status:</strong> {status}
-        </div>
-      </div>
-
+    <div>
+      <AdminDashboardDiv user={user} />
     </div>
+    
   );
 };
 
